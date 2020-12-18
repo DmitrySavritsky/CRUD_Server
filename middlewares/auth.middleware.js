70fbe4f3a0043bdcfef0238661f993a586b23478
DmitrySavritsky <dmitry.savritsky@yandex.ru>
2020-12-18 10:47:57 +0300

    Added verification of user existance in auth module

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,10 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-const auth = function (req,res,next){
+async function userExists(login){
+    const userSchema = require("../models/user.model.js");
+    const user = await userSchema.findOne({name : login});
+    return (user !== null && user !== undefined); 
+}
+
+const auth = async function (req,res,next){
     try{
         const [strategy,token] = req.headers.authorization.split(" ");
         const result = jwt.verify(token, "veryStrongSecretKey");
         req.login = result.login;
+
+        const isExists = await userExists(result.login);
+        if(!isExists){
+            throw new Error("User not exists!");
+        }
         next();
     }
     catch(err){
